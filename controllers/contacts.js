e94c130ad5495a67e1fcafab30229fa7ed6e7f03
wjdavie <126302959+wjdavie@users.noreply.github.com>
2026-02-04 21:15:09 -0500

    Created the functions to create, edit, and delete a contact. Updated the routes and .rest file.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -22,4 +22,63 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  const response = await mongodb
+  .getDb()
+  .db()
+  .collection('contacts')
+  .insertOne(contact);
+
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json({ error: 'An error occured while creating the contact'});
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  const response = await mongodb
+  .getDb()
+  .db()
+  .collection('contacts')
+  .replaceOne({ _id: userId }, contact);
+
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json({ error: 'An error occured while updating the contact'});
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .deleteOne({ _id: userId }, contact);
+
+  if (response.deletedCount > 0) {
+    res.status(200).json({ message: 'Contact deleted successfully.' });
+  } else {
+    res.status(500).json({ error: 'An error occured while deleting the contact'});
+  }
+};
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
